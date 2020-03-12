63b120c12078fe994aabb31714d1b72e38ee3d71
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-12 21:42:09 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,19 @@
+class Appointment
+  attr_accessor :date, :patient, :doctor
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(date, patient, doctor)
+    self.date = date
+    self.patient = patient
+    self.doctor = doctor
+
+    @@all << self
+  end
+
+
+end
